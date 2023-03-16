f6ea62ccaf109747c1abaf59745a9f0edb406212
m.gainatullin <m.gaynatullin@mail.ru>
2023-03-16 20:38:32 +0300

    get rid of seeding

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,26 +1,25 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { Channel } from '../channel/channel.entity';
-import { Factory } from 'nestjs-seeder';
 
 @Entity('users')
 export class User {
-  @Factory((faker) => faker.name.fullName())
+  // @Factory((faker) => faker.name.fullName())
   @ApiProperty({ example: 0, description: 'Unique identifier' })
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Factory((faker, ctx) => faker.helpers.unique(faker.internet.domainName))
+  // @Factory((faker, fakectx) => faker.helpers.unique(faker.internet.domainName))
   @ApiProperty({ example: 'string', description: 'Username' })
   @Column({ type: 'varchar', length: 55 })
   username: string;
 
-  @Factory((faker, ctx) => faker.internet.email(ctx.name))
+  // @Factory((faker, ctx) => faker.internet.email(ctx.name))
   @ApiProperty({ example: 'string', description: 'User email' })
   @Column()
   email: string;
 
-  @Factory((faker, ctx) => faker.internet.password())
+  // @Factory((faker, ctx) => faker.internet.password())
   @Column({ select: false })
   passwordHash: string;
 
